764077151a8e6014ec334574f32c6474e658fa40
Botond Diviki-Nagy <boti0909@gmail.com>
2019-04-26 15:17:57 +0200

    added light sequence of many countries

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 console.log('he');
 
+var cycles = {
+  gb: ['.red', '.red,.yellow', '.green', '.green', '.yellow'],
+  hu: ['.red', '.red,.yellow', '.green', '.green', '.yellow'],
+  de: ['.red', '.red,.yellow', '.green', '.green', '.yellow'],
+  ch: ['.red', '.red,.yellow', '.green', '.green', '.yellow'],
+  fi: ['.red', '.red,.yellow', '.green', '.green', '.yellow'],
+  tr: ['.red', '.red,.yellow', '.green', '.green', '.yellow'],
+  rs: ['.red', '.red,.yellow', '.green', '.green,.blink', '.yellow'],
+  ua: ['.red', '.red,.yellow', '.green', '.green,.blink', '.yellow'],
+  gr: ['.red', '.red', '.green', '.green', '.yellow'],
+  ie: ['.red', '.red', '.green', '.green', '.yellow'],
+  fr: ['.red', '.red', '.green', '.green', '.yellow'],
+  it: ['.red', '.red', '.green', '.green', '.yellow'],
+  es: ['.red', '.red', '.green', '.green', '.yellow'],
+  ru: ['.red', '.yellow', '.green', '.green,.blink', '.yellow']
+};
+
 var map;
-var countries;
+var countries = [];
 var state = 0;
 
 var light = document.querySelector("#light").innerHTML;
@@ -30,37 +47,55 @@ function main() {
   d3.select(map).selectAll(".country")
     .each(function() {
       fillBlue(this);
-      console.log(this.id);
+      countries.push(this.id);
       // d3.select(map).select("#"+this.id+"_light").select(".red").style("fill", "gray");
     });
 
-  d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
+  // d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
 
-  window.setInterval(function() {
-    if (state == 0) {
-      d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
-      d3.select(map).selectAll(".red").style("fill", "red");
-      state = 1;
+  // window.setInterval(function() {
+  //   if (state == 0) {
+  //     d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
+  //     d3.select(map).selectAll(".red").style("fill", "red");
+  //     state = 1;
+  //   }
+  //   else if (state == 1) {
+  //     d3.select(map).selectAll(".yellow").style("fill", "yellow");
+  //     state = 2;
+  //   }
+  //   else if (state == 2) {
+  //     d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
+  //     d3.select(map).selectAll(".green").style("fill", "green");
+  //     state = 3;
+  //   }
+  //   else if (state == 3) {
+  //     d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
+  //     d3.select(map).selectAll(".yellow").style("fill", "yellow");
+  //     state = 0;
+  //   }
+  // }, 3000);
+
+
+  function animate() {
+    d3.select(map).selectAll(".red,.green,.yellow").classed("off", true);
+    d3.select(map).selectAll(".red,.green,.yellow").classed("blink", false);
+    for (var i = 0; i < countries.length; i++) {
+      if (cycles.hasOwnProperty(countries[i])) {
+        d3.select(map).select("#"+countries[i]+"_light").selectAll(cycles[countries[i]][state]).classed("off", false);
+        if (cycles[countries[i]][state].includes("blink")) {
+          d3.select(map).select("#"+countries[i]+"_light").selectAll(cycles[countries[i]][state]).classed("blink", true);
+        }
+      }
     }
-    else if (state == 1) {
-      d3.select(map).selectAll(".yellow").style("fill", "yellow");
-      state = 2;
-    }
-    else if (state == 2) {
-      d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
-      d3.select(map).selectAll(".green").style("fill", "green");
-      state = 3;
-    }
-    else if (state == 3) {
-      d3.select(map).selectAll(".red,.green,.yellow").style("fill", "gray");
-      d3.select(map).selectAll(".yellow").style("fill", "yellow");
-      state = 0;
-    }
-  }, 2000);
+    window.setTimeout(animate, [5000, 2000, 3000, 4000, 2000][state]);
+    state = (state + 1) % 5;
+  }
+  animate();
 
   function fillBlue(tgt) {
     // document.body.innerHTML += light;
     // console.log("he");
+    if (!cycles.hasOwnProperty(tgt.id)) { return; }
     var c = tgt.getBoundingClientRect();
     var x = c.x + (c.width / 2);
     var y = c.y + (c.height / 2);
